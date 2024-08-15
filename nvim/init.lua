d3e39934c3d2901b055b3791775fcf099984eae6
Matthew Nesbitt <mnesbitt@evertz.com>
2024-08-15 09:24:21 -0400

    changed leader key to space and made telescope use that

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,15 +13,20 @@ Plug('lewis6991/gitsigns.nvim')
 
 vim.call('plug#end')
 
+vim.g.mapleader = " "
+vim.opt.updatetime = 300
+
+-- coc auto install extensions
 vim.g.coc_global_extensions = {'coc-json', 'coc-sh', 'coc-pyright', 'coc-zig', 'coc-cmake'}
 
-vim.opt.updatetime = 300
+-- setup left side columm
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes:2' 
 
 local keyset = vim.keymap.set
 
+-- catppuccin setup
 require("catppuccin").setup({
 	flavour = "mocha",
 	styles = {
@@ -57,23 +62,27 @@ require("catppuccin").setup({
 
 vim.cmd('colorscheme catppuccin')
 
+-- sidebar git change indicators
 require('gitsigns').setup {
 	signs = {
 		sign_priority = 1000,
 	},
 }
 
+-- vertical tabspace lines
 require("ibl").setup()
-require('gitsigns').setup()
 
+-- autocomplete
 function _G.check_back_space()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
+-- autocomplete keymap
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
 keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#confirm() : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 
+-- highlight symbol under curson
 vim.api.nvim_create_augroup("CocGroup", {})
 vim.api.nvim_create_autocmd("CursorHold", {
     group = "CocGroup",
@@ -81,5 +90,8 @@ vim.api.nvim_create_autocmd("CursorHold", {
     desc = "Highlight symbol under cursor on CursorHold"
 })
 
-keyset('n',  '<S-f>', ':Telescope find_files <CR>')
+-- telescope keybinds
+keyset('n',  '<leader>f', ':Telescope find_files <CR>')
+keyset('n',  '<leader>g', ':Telescope live_grep <CR>')
+keyset('n',  '<leader>c', ':Telescope grep_string <CR>')
 
