2c09bca8b5bbcdf7188738ffdab6c503050bd938
Matthew Nesbitt <mnesbitt@evertz.com>
2025-10-22 10:28:33 -0400

    tab formatting updates for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,8 +47,11 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes:2'
 
 -- formatting opts
+vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
 
 -- Set filetypes as typescriptreact for .tsx and .jsx files
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
