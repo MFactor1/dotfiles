aa685f5117c6a1ac5fdc9901ef797c6678477a6c
MFactor1 <mingsqu@gmail.com>
2026-03-29 12:24:08 -0400

    switch to using AWWW

---
+++ hypr/awww_gradient_loop.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+INTERVAL=60
+
+if [ $# -lt 1 ] || [ ! -d "$1" ]; then
+	printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
+	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified)." "$DEFAULT_INTERVAL"
+	exit 1
+fi
+
+while true; do
+	find "$1" -type f \
+	| while read -r img; do
+		echo "$(</dev/urandom tr -dc a-zA-Z0-9 | head -c 8):$img"
+	done \
+	| sort -n | cut -d':' -f2- \
+	| while read -r img; do
+		swww img \
+			--resize crop \
+			--transition-type any \
+			--transition-step 2 \
+			--transition-fps 240 \
+			"$img"
+		sleep "${2:-$DEFAULT_INTERVAL}"
+	done
+done
+
+
